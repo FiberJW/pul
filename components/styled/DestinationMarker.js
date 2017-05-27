9a4bd91ef6740804aedf1a531e23bd90551b0da9
Juwan Wheatley <datwheat@gmail.com>
2017-05-27 18:57:17 -0400

    destination marker uses fiber-ev

--- components/styled/DestinationMarker.js
+++ components/styled/DestinationMarker.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components/native";
 import colors from "kolors";
-import ElevatedView from "react-native-elevated-view";
+import ElevatedView from "fiber-react-native-elevated-view";
 
 const Outer = styled(ElevatedView)`
   height: 24;
