b5666908ab3e60bf7595e991e42ac8e5ba388fca
Juwan Wheatley <datwheat@gmail.com>
2017-05-27 22:40:43 -0400

    veoe

--- components/ShareButton.js
+++ components/ShareButton.js
@@ -5,15 +5,16 @@ import colors from "kolors";
 
 const ShareButton = () => (
   <TouchableOpacity
-    onPress={() => Share.share({
-      title: "Let's ride!",
-      message: "Upgrade your school experience with PÜL!\n" +
-        "https://play.google.com/store/apps/details?id=io.github.datwheat.pul\n" +
-        "https://itunes.apple.com/us/app/p%C3%BCl-carpooling-for-students-by-students/id1196047562?ls=1&mt=8"
-    })}
+    onPress={() =>
+      Share.share({
+        title: "Let's ride!",
+        message: "Upgrade your school experience with PÜL!\n" +
+          "https://play.google.com/store/apps/details?id=io.github.datwheat.pul\n" +
+          "https://itunes.apple.com/us/app/p%C3%BCl-carpooling-for-students-by-students/id1196047562?ls=1&mt=8"
+      })}
     style={styles.container}
   >
-    <Icon name="share" outline size={24} color={colors.black} />
+    <Icon name="share" size={24} color={colors.black} />
   </TouchableOpacity>
 );
 
