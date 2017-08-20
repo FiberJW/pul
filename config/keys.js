8728e8999804edd40dea4c14efd8b079f7842e77
Juwan Wheatley <datwheat@gmail.com>
2017-08-20 13:29:39 -0400

    the final push (hopefully)

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC8twx77HQ80Cf9VoFtdkVsTl0dsf5OZok",
+  authDomain: "pulapp-80d33.firebaseapp.com",
+  databaseURL: "https://pulapp-80d33.firebaseio.com",
+  storageBucket: "pulapp-80d33.appspot.com",
+  messagingSenderId: "549128301559"
+};
+
+export const lyftClientId = "vIW8hjXUoqPU";
+
+export const googleApiKey = "AIzaSyBisYcfer0ncVkLuHnb7XSdet4l8wBXR74";
+
+export const sentryURL =
+  "https://a63f65fabe714c39bc843d0442a8b86b@sentry.io/124472";
